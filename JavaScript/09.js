12adebc43e501bf46679403fa4f053508133dfcf
fedevargas110 <fedevargas110@gmail.com>
2022-09-26 21:43:54 -0300

    Booleans

---
+++ JavaScript/09.js
@@ -0,0 +1,13 @@
+// Booleans sireven para cuando esta autenticado el usario mostrar mas cosas o no
+
+const boolean = true;
+const boolean2 = false;
+
+console.log(typeof boolean);
+console.log(boolean2);
+
+// typeof sirve para ver que tipo de dato estoy utilizando string, boolen, etc.
+
+// Creando un costructor de booleans
+const boolean3 = new Boolean(true);
+console.log(boolean3);
