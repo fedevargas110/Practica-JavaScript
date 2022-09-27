c4ae7a1e159f910366d049b7f1f6f9e6fc1d7940
fedevargas110 <fedevargas110@gmail.com>
2022-09-26 21:24:54 -0300

    Orden de las operaciones e Incrementos

---
+++ JavaScript/07.js
@@ -0,0 +1,32 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) * 2; // El orden en que se ejecutan las operaciones las maramos nosotros con parentesis o sin parentesis
+resultado = (20 + 200 + 300)* .2; // Estamos aplicando el 20% del resultado de las sumas
+
+// Esto sirve para paginas con carrito.
+
+console.log(resultado);
+
+//Incrmentos por ejemlo matas en un juego y subis de puntos o de likes en una foto publicada.
+
+let acumulador;
+
+acumulador = 0;
+
+console.log(acumulador);
+// Para que suba de 1 en 1
+acumulador++; // 1
+++acumulador; // 2
+console.log(acumulador);
+
+//Para que baje de 1 en 1;
+acumulador--;// 1
+--acumulador; // 0
+console.log(acumulador);
+
+//Para subir dependiendo lo q matemos en el juego
+
+acumulador += 10;
+console.log(acumulador);
