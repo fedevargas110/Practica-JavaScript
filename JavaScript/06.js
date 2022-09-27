980824a234c26ceac4ccc406eb3101f3919f331c
fedevargas110 <fedevargas110@gmail.com>
2022-09-26 21:06:55 -0300

    Ejercicios con el objeto Math

---
+++ JavaScript/06.js
@@ -0,0 +1,17 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI; // muestra el resultado de PI
+resultado = Math.random(); // genera un numero random
+resultado = Math.round(2.5); // Redondea el numero a partir de .5 hacia arriba y .4 hacia abajo
+resultado = Math.ceil(2.000000001); // redondea siempre hacia arriba.
+resultado = Math.floor(2.9); // redondea siempre hacia abajo.
+resultado = Math.sqrt(144); // Pedimos la raiz cuadrada del numero en parentesis  
+resultado = Math.abs(-200); // Pedimos el numero absoluto del numero en parentesis
+resultado = Math.min(3, 2, 1, 7, 8); // Muestra el valor minimo de la cadena
+resultado = Math.max(3, 2, 1, 7, 8);// Muestra el valor maximo de la cadena
+resultado = Math.floor(Math.random() * 400); // pedimos que muestre un numero random redondeado hacia abajo entre 
+//0 y 400.
+
+console.log(resultado);
