c2c35e60bc03f8fe908f931d862d6db420553968
fedevargas110 <fedevargas110@gmail.com>
2022-09-26 21:34:51 -0300

    Concatenacion de Strings y Template de Strings

---
+++ JavaScript/08.js
@@ -0,0 +1,12 @@
+const nombre = 'fede';
+const email = 'fede@gmail.com';
+
+
+console.log(nombre);
+console.log(email);
+
+// Concatenacion para mostrar en una sola linea
+console.log('Nombre del Cliente: ' + nombre + ', Email: ' + email);
+
+// Template Strings o String Literals sirev para ostrar en una sola linea pero diferente sintaxis
+console.log(`Nombre del Cliente: ${nombre}, email: ${email}`);
