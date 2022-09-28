28ac96fc370820ef9fd4130efc4ab30bf04be133
fedevargas110 <fedevargas110@gmail.com>
2022-09-28 20:52:49 -0300

    Dif entre funcion y metodo

---
+++ JavaScript/17.js
@@ -0,0 +1,7 @@
+// Diferencia entre Metodos y Funciones
+
+const n1 = 20;
+const n2 = '20';
+
+console.log( parseInt(n2)); // Esto es una funcion porque esta la funcion seguida de la varible en parentesis.
+console.log(n2.toString()); // Esto es un metodo porque esta primero la variable seguido de la funcion.
