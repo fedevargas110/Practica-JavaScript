e6ff3c0f1d58168baed54cf3cbd68aee7166881a
fedevargas110 <fedevargas110@gmail.com>
2022-10-11 22:42:46 -0300

    Herencia de Clases

--- JavaScript/28.js
+++ JavaScript/28.js
@@ -16,6 +16,21 @@ class Producto{
 }
 
 const producto2 = new Producto('Monitor Curvo', 300);
+// Herencia con extends
+class Libro extends Producto{
+    constructor(nombre, precio, isbn){
+        super(nombre, precio);// hace referencia a los elements de la clase padre.
+        this.isbn = isbn;
+    }
+    formatearProducto(){
+        return `${super.formatearProducto()} y su isbn es: ${this.isbn}`
+    }
+}
+
+const libro = new Libro('Harry Popoter', 120, '8567463131');
 
 console.log(producto2);
+console.log(producto2.formatearProducto());
 console.log(producto2.getPrecio());
+console.log(libro);
+console.log(libro.formatearProducto());
