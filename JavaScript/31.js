b9b385486f056b00cffd402cd7aa9492d8e6f2bf
fedevargas110 <fedevargas110@gmail.com>
2022-10-12 00:09:08 -0300

    Notification API

---
+++ JavaScript/31.js
@@ -0,0 +1,17 @@
+// Notification API
+
+
+const boton = document.querySelector('#boton');
+
+boton.addEventListener('click', () =>{
+    Notification.requestPermission()
+        .then(resultado => console.log(`El resultado es: ${resultado}`))
+});
+
+if (Notification.permission == 'granted'){
+
+    new Notification('Esta es una notificacion', {
+        icon: 'img/cap.png',
+        body: 'Codigo con Fede'
+    });
+}
