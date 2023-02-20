ccb37a490d7792fb1affb1d3e945159bcba38ed0
fedevargas110 <fedevargas110@gmail.com>
2023-02-20 17:13:20 -0300

    Variables con var

---
+++ JavaScript/Variables.js
@@ -0,0 +1,26 @@
+//Variables en JS forma anterior con var
+
+var nombre = 'Pepe'; //Variable que asignamos valor
+
+var apellido; //Variable a la cual le asignamos mas adelante valor
+
+apellido = true;
+
+var nombre1 = 'Vicki', // Forma de crear bvvarias variables en una misma
+    apellido1 = 'Levis',
+    edad = 20;
+
+// Las variables no se pueden nombrar con un numero, asterico, etc al principio
+// si al medio o al ultimo.
+
+//Forma de mostrar valores por consola
+
+console.log(nombre);
+
+//Estilos de variables
+var nombre_primario = 'Fede' // under_score tambien se usa
+var nombreSecundario = 'Vargas' // camelCase mas usada
+var EdadSujeto = 21 // PascalCase definir clases
+var domiciliosujeto; // lowercase no se usa
+
+
