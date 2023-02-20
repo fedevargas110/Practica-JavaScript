95c92244c3ace3de3371d5901b0c9ea3651a7c56
fedevargas110 <fedevargas110@gmail.com>
2023-02-20 17:19:32 -0300

    Variables con let

---
+++ JavaScript/Variables_let.js
@@ -0,0 +1,24 @@
+//Variables en JS forma nueva con let
+
+let nombre = 'Pepe'; //Variable que asignamos valor
+
+let apellido; //Variable a la cual le asignamos mas adelante valor
+
+apellido = true;
+
+let nombre1 = 'Vicki', // Forma de crear bvvarias variables en una misma
+    apellido1 = 'Levis',
+    edad = 20;
+
+// Las variables no se pueden nombrar con un numero, asterico, etc al principio
+// si al medio o al ultimo.
+
+//Forma de mostrar valores por consola
+
+console.log(nombre);
+
+//Estilos de variables
+let nombre_primario = 'Fede' // under_score tambien se usa
+let nombreSecundario = 'Vargas' // camelCase mas usada
+let EdadSujeto = 21 // PascalCase definir clases
+let domiciliosujeto; // lowercase no se usa
