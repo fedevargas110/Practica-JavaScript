872e605a1c4ca5f2fa66006627b1246b37fb8fca
fedevargas110 <fedevargas110@gmail.com>
2023-02-20 17:39:06 -0300

    Variables con const

---
+++ JavaScript/Variables_const.js
@@ -0,0 +1,24 @@
+//Variables en JS forma nueva con const
+
+const nombre = 'Pepe'; //Variable que asignamos valor
+
+const apellido = 'OSwald' //Variable a la cual le asignamos mas adelante valor
+
+
+
+const nombre1 = 'Vicki', // Forma de crear bvvarias variables en una misma
+    apellido1 = 'Levis',
+    edad = 20;
+
+// Las variables no se pueden nombrar con un numero, asterico, etc al principio
+// si al medio o al ultimo.
+
+//Forma de mostrar valores por consola
+
+console.log(nombre);
+
+//Estilos de variables
+const nombre_primario = 'Fede' // under_score tambien se usa
+const nombreSecundario = 'Vargas' // camelCase mas usada
+const EdadSujeto = 21 // PascalCase definir clases
+const domiciliosujeto = 'Ricardo Rojas'; // lowercase no se usa
