2702b90eeee119a2e8be17403722448b0fbebbb4
fedevargas110 <fedevargas110@gmail.com>
2023-02-21 23:51:09 -0300

    Orden de Operaciones e Incrementos

---
+++ JavaScript/Orden_Operaciones.js
@@ -0,0 +1,30 @@
+// Orden de las operaciones
+
+
+let resultado;
+
+resultado = (20+30) * 2; // El orden en que se ejecutan las operaciones las marcamos nosotros aplicando parentesis
+resultado = (200 + 300 + 500) * .2; // Aplicamos el descuento del 20%
+
+//console.log(resultado);
+
+// Incrementos
+
+
+// se utiliza para subir de likes o en juegos cuando subis de puntos
+
+
+let puntaje = 10;
+
+// Incremento en 1
+puntaje++; //11 muestra puntaje y despues incrementa
+++puntaje; // 12 incrementa y luego lo muestra
+
+// Bajar puntaje 
+puntaje--; // 11
+--puntaje; //10
+
+// Incremento de x
+puntaje += 20; //Incrementa en base a la cantidad que pongamos
+
+console.log(puntaje);
