61d3b0acb02dce9bf07a063991968ca38daacf43
fedevargas110 <fedevargas110@gmail.com>
2023-02-21 23:25:56 -0300

    Objeto Math

---
+++ JavaScript/Objeto_Math.js
@@ -0,0 +1,15 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI; // 3.14....
+resultado = Math.round(2.4) // redondear el numero entre parentesis
+resultado = Math.ceil(2.2) // redondea hacia ARRIBA siempre
+resultado = Math.floor(2.8) // redondea hacia ABAJO siempre
+resultado = Math.sqrt(25) // raiz cuadrada del numero en parentesis
+resultado = Math.abs(-200) // numero absoluto
+resultado = Math.min(2, 5, 3, 10); // el minimo de la lista
+resultado = Math.max(2, 5, 3, 10); // el maximo de la lista
+resultado = Math.random();
+
+console.log(resultado);
