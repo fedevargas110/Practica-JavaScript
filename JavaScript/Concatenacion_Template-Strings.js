c6f4a90541cfba2f33082ee8405190c544e96fad
fedevargas110 <fedevargas110@gmail.com>
2023-02-22 00:00:42 -0300

    Concatenacion y Template Strings

---
+++ JavaScript/Concatenacion_Template-Strings.js
@@ -0,0 +1,11 @@
+const nombre = 'Fede';
+const email = 'fedevargas@gmail.com';
+
+// Concatenacion
+
+console.log(nombre + ' ' + email); // concatenar dos o mas variables
+
+//Template Strings - String Literals
+
+console.log(`nombre: ${nombre} email: ${email}`);
+
